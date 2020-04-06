e0cb6d3835e87c45675cf0858add678b0ea0777c
Kevin Qin <delxiaopang@gmail.com>
2020-04-05 17:35:52 -0700

    Allow Rocket rotate

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,15 +23,23 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
+            rigidBody.AddRelativeForce(100*Vector3.up);
+        } else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("rotate left");
+            transform.Rotate(10*Vector3.forward);
         } else if (Input.GetKey(KeyCode.D))
         {
-            print("rotate right");
+            transform.Rotate(-10*Vector3.forward);
         }
     }
 }
